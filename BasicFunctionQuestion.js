6729393ba3d30292cb27264492cd2166f52c5253
PrachiGupta11 <148103787+PrachiGupta11@users.noreply.github.com>
2023-11-04 13:01:39 +0530

    Uploaded javascript html and js file

---
+++ BasicFunctionQuestion.js
@@ -0,0 +1,17 @@
+function AddTwoNumber()
+{
+    let a=4;
+    let b=5;
+    let sum=a+b;
+    console.log("Sum of 4 and 5 is:"+sum);
+}
+AddTwoNumber();
+
+function DifferenceTwoNumbers()
+{
+    let a=10;
+    let b=5;
+    let diff=a-b;
+    console.log("difference between 10 and 5 is:"+diff);
+}
+DifferenceTwoNumbers();
