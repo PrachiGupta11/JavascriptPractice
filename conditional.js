fd738043bce4adb28815fc8855f6d4084dc98b04
PrachiGupta11 <148103787+PrachiGupta11@users.noreply.github.com>
2023-11-04 20:32:41 +0530

    If Else conditional JS file

---
+++ conditional.js
@@ -0,0 +1,18 @@
+function ifelsecondition(value)
+{
+    
+    if(value=='A')
+    {
+        console.log("Apple");
+    }
+    else if(value=='B')
+    {
+        console.log("Banana");
+    }
+    else{
+        console.log("I don't know");
+    }
+}
+ifelsecondition('A');
+ifelsecondition('B');
+ifelsecondition('C');
