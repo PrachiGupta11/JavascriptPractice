aee2fe43a2320c81a39d075dbb78ae6670a0b782
PrachiGupta11 <148103787+PrachiGupta11@users.noreply.github.com>
2023-11-05 20:31:54 +0530

    Operators JS file

---
+++ operators.js
@@ -0,0 +1,84 @@
+// let num=prompt("Enter number:");
+function fizzbuzz(num)
+{
+    if(num%3==0)
+    {
+        console.log("Fizz");
+    }
+    else if(num%5==0)
+    {
+        console.log("buzz");
+    }
+    else if(num%3==0 && num%5==0)
+    {
+        console.log("fizz buzz");
+    }
+    else if(num%3==0 || num%5==0)
+    {
+        console.log("buzz Fizz");
+    }
+    else{
+        console.log("undefined");
+    }
+}
+fizzbuzz(9);
+fizzbuzz(35);
+fizzbuzz(15);
+fizzbuzz(55);
+fizzbuzz(91);
+
+function operators(num)
+{
+    if(num<0)
+    {
+        console.log("Negative");
+    }
+    else if(num>0)
+    {
+        console.log("Positive");
+    }
+    else if(num==0)
+    {
+        console.log("number is zero");
+    }
+}
+operators(87);
+operators(0);
+operators(-4);
+
+let num1=prompt("Enter first number:");
+let num2=prompt("Enter second number:");
+
+function operators2(num1, num2){
+    
+ // when we take input from prompt that pass string, so we can convert string by using "Number" or "parseInt",
+  //  it is only happen when we use "+" operator, not other arithmetic operator.
+    let sum=(Number(num1) + Number(num2));   
+    console.log("sum is:",sum);
+
+    let sum1=(12+10);
+    console.log("sum is:",sum1);
+
+    let diff=num1-num2;
+    console.log("differnce is:",diff);
+
+    let mul=num1*num2;
+    console.log("Product is:",mul);
+
+    let quo=num1/num2;
+    console.log("Quotient is:",quo);
+
+    if(num1>num2)
+    {
+        console.log("number 1 is greater than number second");
+    }
+    else if(num1<num2)
+    {
+        console.log("number 1 is less than number second");
+    }
+    else if(num1==num2)
+    {
+        console.log("number 1 is equal to number second");
+    }
+}
+operators2(num1,num2);
