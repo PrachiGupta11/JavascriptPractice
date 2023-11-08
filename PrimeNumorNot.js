27ec3509642fe540e1d1dda6373a04f449164fd9
PrachiGupta11 <148103787+PrachiGupta11@users.noreply.github.com>
2023-11-08 20:34:47 +0530

    uploaded

---
+++ PrimeNumorNot.js
@@ -0,0 +1,13 @@
+function prime(num)
+{
+    for(let i=2; i<num;i++){
+        if(num%2==0)
+        {
+    console.log("Not Prime Number");
+        }
+        else{
+            console.log(" prime number");
+        }
+    }
+}
+prime(7);
